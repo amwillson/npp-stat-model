4dd63b2ce6c975ac07071b8278a8f7919a576f4b
Alyssa Willson <awillso2@nd.edu>
2020-05-27 11:41:07 -0400

    Script 1 commented & data added

---
+++ data/SYLVANIA/sylvania2018_TR_readme.txt
@@ -0,0 +1,111 @@
+Readme for TSCA from PALEON 2018 Sampling
+Sylvania wilderness, UP Michigan
+GPS Coordinates: 46.24262 N, 89.34400 W
+Sample Date: 7/3/18
+ 
+ Major Species present: TSCA--Tsuga Canadensis
+ 						BEAL--Betula allegheniensis
+ 						ACSA--Acer saccharum
+ 						THOC--Thuja occidentalis
+ 
+Note: this plot does not overlap with previous 2012 plot from camp PALEON.
+	  To better represent the old-growth forest characteristics for which Sylvania is known,
+	  we moved the plot further away from the tower. This increases TSCA and should align better
+	  with how Sylvania is being categorized in other PALEON analyses. 
+ 
+ 
+TSCA Rundown:
+				Number of dated series        74
+				Master series 1639 2018  380 yrs
+				Total rings in all series   9877
+				Total dated rings checked   9791
+				Series intercorrelation    0.683
+				Average mean sensitivity   0.366
+				Segments, possible problems    8 
+				Mean length of series      133.0
+
+TSCA Files: TSCA_final/ folder
+	earlywood measurements:	seh_tsca_ew.rwl
+	latewood measurements:	seh_tsca_lww.rwl	
+	total ring measurements:sehtsca13.rwl
+	crossdating statistics:	CHK13COF.OUT
+				
+TSCA Cores with No Measurements:
+	SEH013A
+	SEH045A
+	SEH073B 
+
+ TSCA Issues:
+ TSCA cores are a little unusual due to the tendency for them to break up towards the interior.  
+ 	Not all rings labeled as missing in the measurements are true locally absent rings. 
+ 	Some breaks exist that were able to be skipped and keep the core as a single unit.
+ 	Details on which cores this affects are below.
+ 	
+ 	
+ 	SEH018B--break from 1867-1871
+ 	SEH026A--break from 1861-1866
+ 	SEH055A--break from 1831-1832
+ 	
+ * Note that SEH015A1 has lots of missing rings, but these are locally absent rings and not breaks.
+ 
+ 
+ 
+ BEAL Rundown:
+			Number of dated series        24
+			Master series 1768 2018  251 yrs
+			Total rings in all series   3498
+			Total dated rings checked   3490
+			Series intercorrelation    0.546
+			Average mean sensitivity   0.387
+			Segments, possible problems   10
+			Mean length of series      145.0
+			
+BEAL Files: BEAL_final/ folder
+	total ring measurements:	seh_beal06.rwl
+	crossdating statistics:		CHK06COF.OUT		
+			
+BEAL Issues: 
+	SEH008A has unusual tree-ring patterns. It does not crossdate well, statistically, but is dated.
+	
+	
+	
+ACSA Rundown:
+			Number of dated series         7
+			Master series 1891 2018  128 yrs
+			Total rings in all series    722
+			Total dated rings checked    721
+			Series intercorrelation    0.562
+			Average mean sensitivity   0.365
+			Segments, possible problems    1
+			Mean length of series      103.0
+			
+ACSA Files: ASCSA_final/ folder
+	total ring measurements:	seh_acsa05.rwl
+	crossdating statistics:		CHK05COF.OUT
+
+ACSA Issues:
+	There are very few ACSA trees in the plot, and this tree species is notoriously difficult to crossdate.
+	The cores I have are dated to the best of my ability given the shallow sample depth.
+	
+	
+
+
+THOC Rundown:
+
+			Number of dated series        23
+			Master series 1846 2018  173 yrs
+			Total rings in all series   2399
+			Total dated rings checked   2377
+			Series intercorrelation    0.579
+			Average mean sensitivity   0.246
+			Segments, possible problems    4
+			Mean length of series      104.0
+			
+THOC Files: THOC_final/ folder
+	earlywood measurements:		seh_thoc_ew.rwl
+	latewood measurements:		seh_thoc_lww.rwl
+	total ring measurements:	sehthoc01.rwl
+	crossdating statistics:		CHK01COF.OUT
+			
+THOC Issues:  No major issues	
+ 
--- r/build_data_SYLVANIA.R
+++ r/build_data_SYLVANIA.R
@@ -1,35 +1,56 @@
 #!/usr/bin/Rscript
+# Load packages to library
 library(plotrix)
 library(dplR)
 library(fields)
 library(reshape2)
 library(plyr)
 
+## Before running this script, download data from the Wiki for your site
+## Then, add these data to the "data" folder in npp-stat-model under a folder for your site
+## Put all rwl files into one subdirectory "rwl"
+## And convert the meta file to CSV if it is not already in that format
+## It wouldn't hurt to also check out these files to make sure you understand any aspects that are unique to that dataset
+
 # source("config_HMC")
-dataDir = '~/Documents/projects/npp-stat-model/data'
+# Data directory, used to streamline data location between sites
+dataDir = 'data/'
+# Site name, must be the same as site-specific directories
 site = 'SYLVANIA'
+# Data and model versions
 dvers = "v0.1"
 mvers = "v0.1"
 
+# Number of plots, specified in data
 nPlots <- 1
+# Conversion factor
 ftPerMeter <- 3.2808399
 
+# Specify first and last years
 lastYear  <- 2015
 firstYear <- 1940
+# Interpolate years between first and last
 years <- firstYear:lastYear
+# Number of years
 nT <- length(years)
 
-rwFiles <- list.files(paste0("data/", site, '/', "rwl"))
+# List of files with tree ring increments
+rwFiles <- list.files(paste0(dataDir, site, '/', "rwl"))
+# In case there are other files in this directory, take only rwl files
 rwFiles <- rwFiles[grep(".rwl$", rwFiles)]
 rwData <- list()
 for(fn in rwFiles) {
     id <- gsub(".rw", "", fn)
+    # Insert the contents of each file into the rwData list
     rwData[[id]] <- t(read.tucson(file.path("data", site, "rwl", fn)))  # rows are tree, cols are times
 }
 
-treeMeta = read.csv("data/SYLVANIA/alexander_sylvania_June2018.csv")
+# Load CSV with tree data (DBH, location within plot, species)
+treeMeta = read.csv(paste0(dataDir, site, '/alexander_sylvania_June2018.csv'))
 
+# Make dataframe from the list of tree ring increments
 incr = ldply(rwData, rbind)
+# Insert core ID's into the data frame
 incr = incr[,c(".id", sort(colnames(incr)[2:ncol(incr)]))]
 rownames(incr) = as.vector(unlist(lapply(rwData, rownames)))
 incr[,1] = rownames(incr)
@@ -40,31 +61,45 @@ incr[,1] = rownames(incr)
 if (!file.exists('data/dump')){
   dir.create('data/dump')
 } 
-  
+
+# Melt tree ring data frame  
 incr_data = melt(incr)
 colnames(incr_data) = c('id', 'year', 'incr')
+# Assign plot to each core in data frame (in this case, all 1)
 incr_data$plot   = rep(1, nrow(incr_data))#as.numeric(substr(incr_data$id, 3, 3))
+# Assign tree ID to each core (= core ID)
 incr_data$TreeID = incr_data$id 
+# Assign ID to each core (= tree, not core, more than 1 core per tree)
 incr_data$id     = as.numeric(substr(incr_data$id, 4, 6))
+# Assign year to each observation (= dating from tree rings)
 incr_data$year = as.vector(incr_data$year)
 
+# Vector of individual tree IDs (removes directionality from ID)
 tree_ids = unique(substr(incr_data$TreeID, 1, 6))
+# Number of trees with cores
 N_trees  = length(tree_ids)
+# Vector sequence from 1 to the total number of trees to ID each core numerically
 stat_ids = seq(1, N_trees)
 
+# Add stat_ids to data frame by matching tree ID's (each tree gets a separate stat ID)
 incr_data$stat_id = stat_ids[match(substr(incr_data$TreeID, 1, 6), tree_ids)]
 for (n in 1:nrow(incr_data)){
   print(n)
+  # Add species to tree ring data by matching species in whole-tree dataset with the ID
   incr_data$taxon[n] = as.vector(treeMeta$species[which(as.numeric(substr(treeMeta$ID, 4, 6)) == incr_data$id[n])])
 }
 
+# Number of species
 N_taxa = length(unique(incr_data$taxon))
+# Data frame of 4-letter species codes and corresponding numbers
 taxaMatch = data.frame(species=sort(unique(incr_data$taxon)), number=seq(1, N_taxa))
 
+# Vector 4-letter species code corresponding to each tree with tree ring observations
 taxon = aggregate(taxon~stat_id, incr_data, unique)[,2]
+# Convert to numbers corresponding to each species
 taxon = taxaMatch$number[match(taxon, taxaMatch$species)]
 
-
+# Vector of plot number corresponding to each tree (in this case, all 1's)
 plot_id = aggregate(plot~stat_id, incr_data, unique)[,2]
 
 ##########################################################################################################################
@@ -72,67 +107,103 @@ plot_id = aggregate(plot~stat_id, incr_data, unique)[,2]
 ##########################################################################################################################
 # incr = incr[,-1]
 
+# Last year of growth observed in tree rings
 year_end = max(as.numeric(incr_data$year), na.rm=TRUE)
+# First year of growth observed in tree rings
 year_start = min(as.numeric(incr_data$year), na.rm=TRUE)
+# Number of years in the series
 N_years = year_end - year_start + 1
+# Interpolate years
 years = seq(year_start, year_end)
 
-# order by tree and year
+# order by tree and year (for each tree ring)
 incr_data = incr_data[order(incr_data$stat_id, incr_data$year),]
+# Remove all NA values (only keep the series for which trees were alive/existed)
 incr_data = incr_data[which(!is.na(incr_data$incr)),]
+# Make units of time in "years since first recorded tree ring"
 incr_data$year = as.numeric(incr_data$year) - year_start + 1
-N_inc   = nrow(incr_data) # number of measurements
+# Number of measurements
+N_inc   = nrow(incr_data)
+# Vector of years for each increment
 m2t     = incr_data$year
+# Vector of tree ID's for each increment
 m2tree  = incr_data$stat_id
+# Vector of plot for each increment (all 1's)
 m2plot  = incr_data$plot
+# Vector of species numbers for each increment
 m2taxon = taxaMatch$number[match(incr_data$taxon, taxaMatch$species)]
+# Vector of increments (actual data)
 Xobs    = incr_data$incr
+# If the increment is 0 (i.e. no growth), make it a really small increment to fit the assuptions of the log transform
 Xobs[Xobs==0] = 0.0001
+# Log transform
 logXobs = log(Xobs)
 
+# Data frame of the first and last year of observation (years corresponding to first and last increment) for each tree
 year_idx = data.frame(year_start=as.numeric(aggregate(year~stat_id, data=incr_data, FUN=min, na.rm=TRUE)[,2]), 
                       year_end=as.numeric(aggregate(year~stat_id, incr_data, max)[,2]))
 # year_idx[,2] = rep(N_years, nrow(year_idx))
 # year_idx = year_idx - year_start + 1
 
 # make pdbh
+# Make data frame of tree ID number from whole-tree dataset, sequential tree ID, and last year of observation
 pdbh = aggregate(year~stat_id+id, incr_data, max, na.rm=TRUE)
+# Order by tree ID number
 pdbh = pdbh[order(pdbh$stat_id),]
 
 for (n in 1:nrow(pdbh)){
+  # Add DBH from whole-tree dataset using ID to match observations to those in increment dataset
   pdbh$dbh[n] = treeMeta$dbh[which(as.numeric(substr(treeMeta$ID, 4, 6)) == pdbh$id[n])]
+  # Same with distance (location within plot)
   pdbh$distance[n] = treeMeta$distance[which(as.numeric(substr(treeMeta$ID, 4, 6)) == pdbh$id[n])]
 }
+# Number of trees with DBH observations
 N_pdbh = nrow(pdbh)
+# Log transform of DBH
 logPDobs = log(pdbh$dbh)
+# Vector of Tree ID's
 pdbh_tree_id = pdbh$stat_id
 # logPDobs[is.na(logPDobs)] = -999
+# Vector of year of observation for each tree
 pdbh_year_id = pdbh$year
+# Vector of locations within plot
 distance = pdbh$distance
 
 idx_stack = data.frame(meas=numeric(0), tree_id=numeric(0), year=numeric(0))
 n = 1
 for (tree in 1:N_trees){
+  # Vector of years of increment observations for a given tree
   year = seq(year_idx[tree,1], year_idx[tree,2])
+  # Vector of observation index for each increment of a given tree
   meas = seq(n, n+length(year)-1)
+  # Increment counter
   n = n + length(year)
+  # Add to a new data frame
   idx_stack = rbind(idx_stack, data.frame(meas=meas, tree_id=rep(tree, length(year)), year=year))
 }
 
+# Find location of first observation for each tree
 idx_tree = which(!duplicated(idx_stack$tree_id))
+# Data frame of first and last observation for each tree
 idx_tree = data.frame(idx_tree, c(idx_tree[-1]-1, nrow(idx_stack)))
 
+# Vector of tree IDs from the new data frame
 x2tree  = idx_stack$tree_id
+# Vector of years from the new data frame
 x2year  = idx_stack$year 
 
+# Number of observations (= increments) in new data frame
 N_vals   = nrow(idx_stack)
 
 meas2x = vector(length=N_inc)
 for (i in 1:N_inc) {
   print(i)
+  # ID number for given tree from increment data frame
   id = incr_data$stat_id[i]
+  # Year for given tree from increment data frame
   year = incr_data$year[i]
 
+  # Find observations with given ID and year in new data frame
   meas2x[i] = which((idx_stack$tree_id == id) & (idx_stack$year == year))
 }
 
@@ -140,15 +211,19 @@ for (i in 1:N_inc) {
 
 pdbh2val = vector(length=N_pdbh)
 for (i in 1:N_pdbh){
+  # ID number for given tree from DBH data frame
   id = pdbh$stat_id[i]
+  # Year for given tree from DBH data frame (always the alst year in this case)
   year = pdbh$year[i]
   
   print(i)
   which((idx_stack$tree_id == id) & (idx_stack$year == year))
   
+  # Find observations with given ID and year in new data frame
   pdbh2val[i] = which((idx_stack$tree_id == id) & (idx_stack$year == year))
 }
 
+# Create site-specific directories and sub-directories
 site_dir <- file.path('sites',site)
 if (!file.exists(site_dir)){
   dir.create(site_dir)
@@ -157,8 +232,10 @@ if (!file.exists(site_dir)){
   dir.create(file.path(site_dir, 'figures'))
 }
 
+# Vector of plot ID for each tree (always 1 in this case)
 plot_id = rep(1, N_trees)
 
+# Save data in RDS file
 saveRDS(list(N_trees=N_trees, 
            N_years=N_years,
            N_vals=N_vals,
